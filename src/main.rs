5d74430b063e8af0eb298a19c6efdbb4e2b01013
Laurence <laurence@robothouse.io>
2023-06-29 11:49:36 +0200

    setup actix web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web_diesel_test::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = "127.0.0.1:8080";
+    let listener = TcpListener::bind(address)?;
+    println!("Starting Actix Web server at: {address}");
+    run(listener)?.await
 }
